b66e68b4f92af2f9f758fee4ea4f449e2b43cafd
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-09-15 14:07:27 +0300

    Add shop page with routing

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -7,13 +7,14 @@ import AdminDashboard from './pages/admin-view/dashboard';
 import AdminProducts from './pages/admin-view/products';
 import AdminOrders from './pages/admin-view/orders';
 import AdminFeatures from './pages/admin-view/features';
+import ShoppingLayout from './components/shopping-view/layout';
+import NotFound from './pages/not-found';
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       {/* common component */}
-      <h1>Header Component</h1>
-
+      {/* <h2>Header Component</h2> */}
       <Routes>
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
@@ -25,6 +26,8 @@ function App() {
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
+        <Route path="/shop" element={<ShoppingLayout />}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
